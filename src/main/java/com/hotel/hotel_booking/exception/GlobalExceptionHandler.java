db3751b2a9a59caed630037ebdc9a3fca3a60174
Gayand-soul <178871041+Gayand-soul@users.noreply.github.com>
2026-05-30 18:31:59 +0200

    Adding GlobalException

--- src/main/java/com/hotel/hotel_booking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hotel/hotel_booking/exception/GlobalExceptionHandler.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //Helper: builds the timestamp, status, messsage map...........
+
     private Map<String, Object> buildError(HttpStatus status, String message){
         Map<String, Object> error = new HashMap<>();
         error.put("timestamp", LocalDateTime.now().toString());
@@ -23,28 +24,28 @@ public class GlobalExceptionHandler {
         return error;
     }
 
-    //409 Conflict................
+
     @ExceptionHandler(RoomFullyBookedException.class)
     public ResponseEntity<Map<String, Object>> handleRoomFully(RoomFullyBookedException ex){
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(buildError(HttpStatus.CONFLICT, ex.getMessage()));
     }
-    //400 Bad Request..............
+
     @ExceptionHandler(GuestCapacityException.class)
     public ResponseEntity<Map<String, Object>> handleCapacity(GuestCapacityException ex){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(buildError(HttpStatus.BAD_REQUEST, ex.getMessage()));
     }
-    //404 NOT FOUND..................
+
     @ExceptionHandler(BookingNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleNotFound(BookingNotFoundException ex){
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(buildError(HttpStatus.NOT_FOUND, ex.getMessage()));
     }
-    //400 Validation Error..............
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex){
 
@@ -53,7 +54,7 @@ public class GlobalExceptionHandler {
                 .getFieldErrors()
                 .stream()
                 .map(err -> err.getField() + ": " + err.getDefaultMessage())
-                .reduce("", (a,b) -> a.isEmpty()? b: a +","+ b);
+                .collect(Collectors.joining(", "));
 
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
